5ca9b2dc87089456470acca4b530fde773c7bf6e
rohitprasain <microscopicrohit@gmail.com>
2020-10-22 19:53:07 +0545

    before hamburger

---
+++ resp.js
@@ -0,0 +1,25 @@
+// burgerimage = document.querySelector('.burger');
+// navbar = document.querySelector('.navbar');
+// navlinks = document.querySelector('.navlinks');
+
+
+// burgerimage.addEventListener('click' , ()=>{
+
+// 	// navbar.classList.toggle('navbarresp');
+
+//     // navlinks.classList.toggle('navlinksresp');
+//     alert("hello");
+
+// })
+
+
+function myfunction(){
+    // alert("hello");
+    var x = document.getElementsByClassName("services");
+    // if (x.style.display === "block") {
+    //     x.style.display = "none";
+    //   } else {
+    //     x.style.display = "block";
+    //   }
+    console.log(x)
+}
